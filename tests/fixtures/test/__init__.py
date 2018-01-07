4fc235f8ba40781576c372ce2dbb573ac674c15e
Casey Jaymes <cjaymes@gmail.com>
2018-01-07 13:42:08 -0500

    added fixture PREFIX

--- tests/fixtures/test/__init__.py
+++ tests/fixtures/test/__init__.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Expatriate.  If not, see <http://www.gnu.org/licenses/>.
 
+PREFIX = 'test'
+
 ELEMENT_MAP = {
     ('http://jaymes.biz/test', 'RootFixture'): 'RootFixture',
     ('http://jaymes.biz/test', 'AttributeFixture'): 'AttributeFixture',
