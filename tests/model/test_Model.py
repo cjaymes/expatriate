1f57fd8a04759775cd694c72e2a816085dd4d6ba
Casey Jaymes <cjaymes@users.noreply.github.com>
2018-01-11 09:40:45 -0500

    test_produce_dict_value_attr

--- tests/model/test_Model.py
+++ tests/model/test_Model.py
@@ -711,17 +711,17 @@ def test_produce_dict_value_nil():
     assert '<test:dict_value_nil id="test1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:nil="true"/>' in xml
     assert '<test:dict_value_nil id="test2">test2</test:dict_value_nil>' in xml
 
-# def test_produce_dict_value_attr():
-#     model = DictElementFixture(local_name='DictElementFixture', namespace='http://jaymes.biz/test', prefix='test')
-#     model.dict_value_attr['test1'] = 'test1'
-#     model.dict_value_attr['test2'] = 'test2'
-#
-#     xml = model.produce().produce()
-#     assert xml.startswith('<test:DictElementFixture')
-#     assert 'xmlns:test="http://jaymes.biz/test"' in xml
-#     assert '<test:dict_value_attr id="test1" value="test1" />' in xml
-#     assert '<test:dict_value_attr id="test2" value="test2" />' in xml
-#
+def test_produce_dict_value_attr():
+    model = DictElementFixture(local_name='DictElementFixture', namespace='http://jaymes.biz/test', prefix='test')
+    model.dict_value_attr['test1'] = 'test1'
+    model.dict_value_attr['test2'] = 'test2'
+
+    xml = model.produce().produce()
+    assert xml.startswith('<test:DictElementFixture')
+    assert 'xmlns:test="http://jaymes.biz/test"' in xml
+    assert '<test:dict_value_attr id="test1" value="test1"/>' in xml
+    assert '<test:dict_value_attr id="test2" value="test2"/>' in xml
+
 # def test_produce_dict_value_type():
 #     model = DictElementFixture(local_name='DictElementFixture', namespace='http://jaymes.biz/test', prefix='test')
 #     model.dict_value_type['test1'] = 'test1'
